89c4a59ffa7f38434e7ce2e8c20e9d350edc0c15
Hoang Anh Tuan <tuan.ha@altplus.com.vn>
2017-06-02 15:37:07 +0700

    update code okr

---
+++ AppCancer/src/Router.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { StackNavigator} from 'react-navigation';
+
+import Home from './screens/Home';
+
+export const HomeStack = StackNavigator({
+    Home_Screen: {
+        screen: Home,
+        navigationOptions: {
+            title: 'Trang chủ',
+        },
+    },
+});
